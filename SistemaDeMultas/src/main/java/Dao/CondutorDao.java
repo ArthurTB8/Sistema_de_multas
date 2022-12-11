f59bcac43087d89eede650a95489e9b1e5b4670a
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-12-11 13:18:09 -0300

    Add files via upload

---
+++ SistemaDeMultas/src/main/java/Dao/CondutorDao.java
@@ -0,0 +1,53 @@
+package Dao;
+
+import Model.Condutor;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+
+public class CondutorDao{
+
+
+    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("mildevs-multas");
+    private static EntityManager manager = factory.createEntityManager();
+
+    public boolean save(Condutor condutorModel) {
+        manager = Persistence.createEntityManagerFactory("mildevs-multas").createEntityManager();
+        manager.getTransaction().begin();
+        manager.persist(condutorModel);
+        manager.getTransaction().commit();
+        manager.close();
+        return true;
+    }
+
+
+    public Condutor getCondutor(String cnh) {
+        Long idCondutor = getIdCondutor(cnh);
+        if (idCondutor == null) {
+            throw  new RuntimeException("Condutor não encontrado");
+        }
+        return manager.find(Condutor.class, idCondutor);
+
+    }
+
+    private Long getIdCondutor(String cnh) {
+        Query query = manager.createNativeQuery("select c.id from condutor c where c.numeroCnh = " + cnh + ";");
+        for (Object o : query.getResultList()) {
+            if (o != null) {
+                return Long.parseLong(o.toString());
+            }
+        }
+        return null;
+    }
+
+    public void atualizarPontuacaoCNH(Condutor condutor) {
+        manager = Persistence.createEntityManagerFactory("mildevs-multas").createEntityManager();
+        manager.getTransaction().begin();
+        manager.merge(condutor);
+        manager.getTransaction().commit();
+        manager.close();
+    }
+
+}
